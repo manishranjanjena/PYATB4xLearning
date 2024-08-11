19c3c500ffdfe8eb5117f0c7ff708d0dd2fbd69e
Manish <manishjena94@gmail.com>
2024-08-11 13:57:16 +0530

    Data Types

---
+++ src/ex_10082024/Lab_004.py
@@ -0,0 +1,57 @@
+age= 65
+print(age)
+#output- 65
+
+age=85
+age=90
+print(age)
+#output- 90
+
+a=10
+_=66
+_=_+1
+print(a,_)
+#output- 10 67
+
+pi=3.14
+name= "Pramod0"
+isMale= True
+
+print(type(pi))
+print(type(name))
+print(type(isMale))
+
+#output- <class 'float'>
+#        <class 'str'>
+#        <class 'bool'>
+
+
+complex_number= 3+5j
+print(type(complex_number))
+print(type(complex_number.real))
+print(type(complex_number.imag))
+#        <class 'complex'>
+#        <class 'float'>
+#        <class 'float'>
+
+
+age=45
+print(type(age))
+age="Manish"
+print(type(age))
+#output- <class 'int'>
+#        <class 'str'>
+
+
+a=5
+b=6
+c=8
+sum=a+b+c
+print(sum)
+sum=sum-5
+#output- 19
+
+x,y,z= 10,"August",2024
+print(x,y,z,sep='/')
+#output- 10/August/2024
+
