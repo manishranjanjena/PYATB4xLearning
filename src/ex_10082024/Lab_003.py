c616077990d497beb4e32da065074464011d7ef7
Manish <manishjena94@gmail.com>
2024-08-11 12:38:12 +0530

    printing max number and keyword lists

---
+++ src/ex_10082024/Lab_003.py
@@ -0,0 +1,16 @@
+print(max(45,58,466))
+#output-466
+
+print(max(56,45,511.51,))
+#output-511.51
+
+'''print(max(695,94,154.55,"Manish"))
+#output- TypeError: '>' not supported between instances of 'str' and 'int'
+'''
+
+import keyword
+print (keyword.kwlist)
+#output- ['False', 'None', 'True', 'and', 'as', 'assert',
+# 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif',
+# 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in',
+# 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
